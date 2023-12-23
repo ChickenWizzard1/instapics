755b50d763e041b5dd3dd9f1df128e053df836ea
ChickenWizzard1 <102372115+ChickenWizzard1@users.noreply.github.com>
2023-12-23 10:15:04 +0100

    Update only_pics.py

--- only_pics.py
+++ only_pics.py
@@ -1,9 +1,20 @@
 import fnmatch
 import os
 
-def delete_files(folder):
+
+
+
+def keep_pictures(folder):
     for file in os.listdir(folder):
         if fnmatch.fnmatch(file, '*.jpg'):
+            pass
+        else:
+            print("Removed", file)
+            os.remove(folder + "/" + file)
+
+def keep_videos(folder):
+    for file in os.listdir(folder):
+        if fnmatch.fnmatch(file, '*.mp4') or fnmatch.fnmatch(file, '*.jpg'):
             pass
         else:
             print("Removed", file)
